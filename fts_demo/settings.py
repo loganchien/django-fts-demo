1e443c4d84e3a5d7a588557b394230ea8b508801
Logan Chien <tzuhsiang.chien@gmail.com>
2018-01-01 09:33:43 +0800

    Change database setting

--- fts_demo/settings.py
+++ fts_demo/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'fts_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '[db_name]',
+        'USER': '[username]',
+        'PASSWORD': '[password]',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
